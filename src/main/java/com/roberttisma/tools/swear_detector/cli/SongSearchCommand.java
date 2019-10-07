7fd5847b688012b13cc270ffb88e638aaea5289b
Robert Tisma <942951+rtisma@users.noreply.github.com>
2019-10-06 22:36:54 -0400

    created 2 thread pools: 1 for term search and other for link search

--- src/main/java/com/roberttisma/tools/swear_detector/cli/SongSearchCommand.java
+++ src/main/java/com/roberttisma/tools/swear_detector/cli/SongSearchCommand.java
@@ -1,8 +1,10 @@
 package com.roberttisma.tools.swear_detector.cli;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import com.roberttisma.tools.swear_detector.Factory;
 import com.roberttisma.tools.swear_detector.model.Lyric;
+import com.roberttisma.tools.swear_detector.web.LyricClient;
 import kong.unirest.Unirest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,10 +22,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static com.google.common.util.concurrent.MoreExecutors.listeningDecorator;
+import static com.google.common.collect.Lists.newArrayList;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.stream.Collectors.toUnmodifiableList;
 import static java.util.stream.Collectors.toUnmodifiableSet;
@@ -48,8 +51,8 @@ public class SongSearchCommand implements Callable<Integer> {
       names = {"-t", "--term"},
       description = "Dictionary file containing swearwords",
       required = true)
-  private String term;
-//  private List<String> terms;
+  private List<String> terms;
+  //  private List<String> terms;
 
   @Option(
       names = {"--uid"},
@@ -73,49 +76,74 @@ public class SongSearchCommand implements Callable<Integer> {
   @Override
   public Integer call() throws Exception {
     val lyricClient = Factory.buildLyricClient(uid, tokenid);
-    val executor = listeningDecorator(newFixedThreadPool(numThreads));
+    val termExecutor = newFixedThreadPool(numThreads);
+    val linkExecutor = newFixedThreadPool(numThreads);
 
     val dictionarySet =
         Files.readAllLines(file.toPath()).stream()
             .map(String::toLowerCase)
             .collect(toUnmodifiableSet());
-    val results = lyricClient.get(term).getResult();
-
-    if (!results.isEmpty()) {
-
-      val futureResults =
-          results.stream()
-              .map(
-                  x ->
-                      executor.submit(
-                          () -> {
-                            String text = Unirest.get(x.getSongLink()).asString().getBody();
-                            Set<String> found =
-                                dictionarySet.stream()
-                                    .filter(text::contains)
-                                    .collect(toUnmodifiableSet());
-                            return SearchResult.builder().lyric(x).contains(found).build();
-                          }))
-              .collect(Collectors.toUnmodifiableList());
-      executor.shutdown();
-      executor.awaitTermination(5, TimeUnit.MINUTES);
-      val responseResults =
-          futureResults.stream()
-              .map(
-                  x -> {
-                    try {
-                      return x.get();
-                    } catch (ExecutionException | InterruptedException e) {
-                      log.error(e.getMessage());
-                      throw new IllegalStateException(e.getMessage());
-                    }
-                  })
-              .filter(x -> !x.getContains().isEmpty())
-              .collect(toUnmodifiableList());
-      val searchResults = SearchResults.builder().term(term).searchResults(responseResults).build();
-      OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValue(System.out, searchResults);
+    val callables = terms.stream()
+        .map( t -> createSearchTermCallable(lyricClient, t, dictionarySet, linkExecutor) )
+        .collect(toUnmodifiableList());
+    val futures = termExecutor.invokeAll(callables);
+    termExecutor.shutdown();
+    linkExecutor.shutdown();
+    termExecutor.awaitTermination(5, TimeUnit.MINUTES);
+    linkExecutor.awaitTermination(5, TimeUnit.MINUTES);
+    val outpu = Lists.<SearchResults>newArrayList();
+    for(val future : futures){
+      val searchResults = future.get();
+      outpu.add(searchResults);
     }
+    OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValue(System.out, outpu);
     return 0;
+  }
+
+  private static Callable<SearchResults> createSearchTermCallable(
+      LyricClient lyricClient, String term, Set<String> dictionarySet, ExecutorService executor) {
+    return new Callable<SearchResults>() {
+
+      @Override
+      public SearchResults call() throws Exception {
+        val results = lyricClient.get(term).getResult();
+
+        if (results.isEmpty()) {
+          return SearchResults.builder().term(term).searchResults(newArrayList()).build();
+        } else {
+          val callables =
+              results.stream()
+                  .map(
+                      x ->
+                          (Callable<SearchResult>)
+                              () -> {
+                                String text = Unirest.get(x.getSongLink()).asString().getBody();
+                                Set<String> found =
+                                    dictionarySet.stream()
+                                        .filter(text::contains)
+                                        .collect(toUnmodifiableSet());
+                                return SearchResult.builder().lyric(x).contains(found).build();
+                              })
+                  .collect(Collectors.toUnmodifiableList());
+          log.info("starting to invoke");
+          val responseResults =
+              executor.invokeAll(callables).stream()
+                  .map(
+                      x -> {
+                        try {
+                          return x.get();
+                        } catch (ExecutionException | InterruptedException e) {
+                          log.error(e.getMessage());
+                          throw new IllegalStateException(e.getMessage());
+                        }
+                      })
+                  .filter(x -> !x.getContains().isEmpty())
+                  .collect(toUnmodifiableList());
+          log.info("done invoking");
+          return SearchResults.builder().term(term).searchResults(responseResults).build();
+        }
+      }
+    };
   }
 
   @Data
